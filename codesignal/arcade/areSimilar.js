949f32747b910519fd763bb6949234fef0bf02a4
Arjith Natarajan <arjith@sigaramtech.com>
2018-08-26 20:23:28 +0530

    Finish unsolved problem in arcade - codeSignal

--- codesignal/arcade/areSimilar.js
+++ codesignal/arcade/areSimilar.js
@@ -1,14 +1,20 @@
 // https://app.codesignal.com/arcade/intro/level-4/xYXfzQmnhBvEKJwXP
 //
 
-const areSimilar = (a, b) => {
+areSimilar = (a, b) => {
   if (a.length !== b.length) return false;
-  let misplaced = 0;
+  let misplaced = 0,
+    x = [],
+    y = [];
   for (var i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) misplaced++;
+    if (a[i] !== b[i]) {
+      x.push(a[i]);
+      y.push(b[i]);
+      misplaced++;
+    }
   }
-  if (misplaced === 2 || misplaced === 0) return true;
-  return false;
+  return !misplaced || (misplaced === 2 && x[0] === y[1] && x[1] === y[0])
+    ? true
+    : false;
 };
-
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
